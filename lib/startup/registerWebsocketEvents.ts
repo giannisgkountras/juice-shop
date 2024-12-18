77a25408f45c305d7eced7bf39cb0d0703eff214
annikola <annikolaprof@gmail.com>
2024-12-18 06:54:01 +0200

    Major 'any' violations fixed

--- lib/startup/registerWebsocketEvents.ts
+++ lib/startup/registerWebsocketEvents.ts
@@ -5,45 +5,47 @@
 
 import config from 'config'
 import * as utils from '../utils'
-import { Server } from 'socket.io'
+import { Server, Socket } from 'socket.io'
 import { notifications, challenges } from '../../data/datacache'
 import * as challengeUtils from '../challengeUtils'
 import * as security from '../insecurity'
 
-let firstConnectedSocket: any = null
+import { Server as HTTPServer } from 'http'  // Import http.Server
+
+let firstConnectedSocket: string | null = null
 
 const globalWithSocketIO = global as typeof globalThis & {
   io: SocketIOClientStatic & Server
 }
 
-const registerWebsocketEvents = (server: any) => {
+const registerWebsocketEvents = (server: HTTPServer) => {
   const io = new Server(server, { cors: { origin: 'http://localhost:4200' } })
   // @ts-expect-error FIXME Type safety issue when setting global socket-io object
   globalWithSocketIO.io = io
 
-  io.on('connection', (socket: any) => {
+  io.on('connection', (socket: Socket) => {
     if (firstConnectedSocket === null) {
       socket.emit('server started')
       firstConnectedSocket = socket.id
     }
 
-    notifications.forEach((notification: any) => {
+    notifications.forEach((notification) => {
       socket.emit('challenge solved', notification)
     })
 
-    socket.on('notification received', (data: any) => {
-      const i = notifications.findIndex(({ flag }: any) => flag === data)
+    socket.on('notification received', (data: string) => {
+      const i = notifications.findIndex(({ flag }) => flag === data)
       if (i > -1) {
         notifications.splice(i, 1)
       }
     })
 
-    socket.on('verifyLocalXssChallenge', (data: any) => {
+    socket.on('verifyLocalXssChallenge', (data: string) => {
       challengeUtils.solveIf(challenges.localXssChallenge, () => { return utils.contains(data, '<iframe src="javascript:alert(`xss`)">') })
       challengeUtils.solveIf(challenges.xssBonusChallenge, () => { return utils.contains(data, config.get('challenges.xssBonusPayload')) })
     })
 
-    socket.on('verifySvgInjectionChallenge', (data: any) => {
+    socket.on('verifySvgInjectionChallenge', (data: string) => {
       challengeUtils.solveIf(challenges.svgInjectionChallenge, () => { return data?.match(/.*\.\.\/\.\.\/\.\.[\w/-]*?\/redirect\?to=https?:\/\/placekitten.com\/(g\/)?[\d]+\/[\d]+.*/) && security.isRedirectAllowed(data) })
     })
 
